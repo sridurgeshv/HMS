9d6488335bede419db33a4bfda4369a8cc9e6297
sridurgeshv <sridurgeshv@gmail.com>
2025-03-05 18:11:18 +0000

    Finally resolved

--- hospital-management-system/frontend/tailwind.config.js
+++ hospital-management-system/frontend/tailwind.config.js
@@ -1,10 +1,7 @@
-// tailwind.config.js
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
