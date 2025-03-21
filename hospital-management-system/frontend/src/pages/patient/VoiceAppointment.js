bb7cd9114f8c0a81f9547daa49aafd5831991df1
Ritika Srivastava <69151779+ritika12df@users.noreply.github.com>
2025-03-21 17:00:07 +0530

    Update VoiceAppointment.js

--- hospital-management-system/frontend/src/pages/patient/VoiceAppointment.js
+++ hospital-management-system/frontend/src/pages/patient/VoiceAppointment.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import axios from "axios";
-//import "./VoiceAppointment.css";
+import "./VoiceAppointment.css";
 
 const VoiceAppointment = ({ patientId }) => {
   const [recording, setRecording] = useState(false);
